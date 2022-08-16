ef0ae75b2a7ab5f519a508723b51e37323b2d0ad
programmermohaimin <you@example.com>
2022-08-16 18:51:58 +0600

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+let form = document.getElementById('form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    
+    let formData = {};
+
+    [...this.elements].forEach(element => {
+        if (element.type !== 'submit') {
+            formData[element] = element.id
+        }
+    })
+    console.log(formData);
+})
